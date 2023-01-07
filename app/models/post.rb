6567a24c2d276de2e8e49bad73e5cad7d701fde2
Kevin Kelley <kkelley622@gmail.com>
2023-01-06 22:13:18 -0600

    commit tree 0d84731c1a5d48a4e30c36bd65094e8ae2579f7b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 } 
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :sufficiently_clickbait
+
+    CLICKBAIT = [
+        /Won't Believe/i, 
+        /Secret/i, 
+        /Top \d/i, 
+        /Guess/i]
+
+    def sufficiently_clickbait
+        if CLICKBAIT.none? { |bait| bait.match title }
+            errors.add(:title, "Must be more clickbaity")
+        end
+    end
 end
